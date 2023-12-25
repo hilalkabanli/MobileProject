8afd26820b6e5725905aaa0cedbd41046372fb46
hilalkabanli <hilal.kabanlii@gmail.com>
2023-12-25 12:51:20 +0300

    conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 android {
@@ -40,6 +41,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:20.0.0")
 
