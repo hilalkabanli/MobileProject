d07e58ccbea0a386ebb09ddd63d34df7745cc8d7
Rumeysa Uçar <ucar.rumeys@gmail.com>
2023-12-28 05:27:10 +0300

    LostAnimalNoticeActivity + LostAnimalViewActivity

--- app/src/main/java/com/example/ceyda/friendlypaws/LostAnimalNoticeActivity.java
+++ app/src/main/java/com/example/ceyda/friendlypaws/LostAnimalNoticeActivity.java
@@ -1,22 +1,178 @@
 package com.example.ceyda.friendlypaws;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.ceyda.friendlypaws.model.Userr;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class LostAnimalNoticeActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_PICK = 2;
+    private ImageView imageViewPet;
+    private EditText editTextAddress;
+    private EditText editTextInfo;
+    private GestureDetector gestureDetector;
+    byte[] imageData;
+
+    private String phone, userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_animal_notice);
+
+        imageViewPet = findViewById(R.id.imageViewPet2);
+        editTextAddress = findViewById(R.id.editTextAddress2);
+        editTextInfo = findViewById(R.id.editTextInfo2);
+
+        fetchUserInformation();  //take user phone Number an ID to add Lost Pet Notice
+
+        Button buttonChoosePhoto = findViewById(R.id.buttonChoosePhoto2);
+        buttonChoosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        Button postbtn = findViewById(R.id.button72);
+        gestureDetector = new GestureDetector(this, new GestureListener(postbtn));
+
+        // Set an onTouchListener for the button
+        postbtn.setOnTouchListener((v, event) -> {
+            gestureDetector.onTouchEvent(event);
+            return true; // Consume the event
+        });
     }
 
-    public void doublePostBtnFunction() {
 
+    public void doublePostBtnFunction() {
+        // Example: Insert data
+        ContentValues values = new ContentValues();
+        values.put(Contract.MyEntry.COLUMN_PICTURE, imageData);
+        values.put(Contract.MyEntry.COLUMN_ADDRESS, editTextAddress.getText().toString());
+        values.put(Contract.MyEntry.COLUMN_INFO, editTextInfo.getText().toString());
+        values.put(Contract.MyEntry.COLUMN_PHONE, phone);
+        values.put(Contract.MyEntry.COLUMN_USER_ID, userID);
+
+        Uri uri = getContentResolver().insert(Contract.MyEntry.CONTENT_URI, values);
+        long newRowId = Long.parseLong(uri.getLastPathSegment());
+        Toast.makeText(LostAnimalNoticeActivity.this, "New Row ID: " + newRowId, Toast.LENGTH_SHORT).show();
+
+        // Example: Query data
+        Cursor cursor = getContentResolver().query(Contract.MyEntry.CONTENT_URI, null, null, null, null);
+
+        // Process the cursor...
+
+        // Close the cursor
+        cursor.close();
+
+        Intent intent = new Intent(LostAnimalNoticeActivity.this, LostAnimalActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void singlePostBtnFunction() {
+        Toast.makeText(LostAnimalNoticeActivity.this, "Are u sure? Then double click ❣" , Toast.LENGTH_SHORT).show();
+    }
 
+    private void openGallery() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, REQUEST_IMAGE_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
+            if (data != null) {
+                // Handle the selected image from the gallery
+                Uri selectedImageUri = data.getData();
+                imageViewPet.setImageURI(selectedImageUri);
+
+                // Now you can convert the Uri to a byte array and store it in your database
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.WEBP, 100, stream);
+
+                    imageData = stream.toByteArray();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void fetchUserInformation() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            String userId = firebaseUser.getUid();
+            //userID = userId;
+
+            DatabaseReference userReference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+            userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Log.d("FirebaseData", "onDataChange triggered");
+                    if (dataSnapshot.exists()) {
+                        Log.d("FirebaseData", "Data exists");
+                        //User data found
+                        Userr user = dataSnapshot.getValue(Userr.class);
+
+                        // Now 'user' contains the information you stored in the database
+                        phone = user.getPhone();
+                        userID = user.getUserId();
+
+                    } else {
+                        // User data not found in the database, handle accordingly
+                        Log.d("FirebaseData", "Data does not exist");
+                        Toast.makeText(LostAnimalNoticeActivity.this, "User data not found.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle errors here
+                    Toast.makeText(LostAnimalNoticeActivity.this, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed(); // This line is important to ensure the default behavior
+        // Your custom behavior, if any
+        Intent intent = new Intent(LostAnimalNoticeActivity.this, LostAnimalActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/ceyda/friendlypaws/LostAnimalViewActivity.java
+++ app/src/main/java/com/example/ceyda/friendlypaws/LostAnimalViewActivity.java
@@ -2,13 +2,165 @@ package com.example.ceyda.friendlypaws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LostAnimalViewActivity extends AppCompatActivity {
+
+    private Button okbtn, deletebtn;
+    private TextView adress_txt, phone_txt, info_txt;
+    private ImageView img_view;
+    private String address, phone, info, userid;
+
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_animal_view);
+
+        okbtn = findViewById(R.id.ok_button);
+        deletebtn = findViewById(R.id.delete_button);
+
+        adress_txt = findViewById(R.id.AddressText);
+        phone_txt = findViewById(R.id.PhoneText);
+        info_txt = findViewById(R.id.InfoText);
+
+        img_view = findViewById(R.id.imageViewPet);
+
+        Intent intent = getIntent();
+        String animal_id = getIntent().getStringExtra("id");
+
+        getOtherInfo(animal_id);
+
+        // Set values to TextViews
+        //idTextView.setText("ID: " + id);
+        adress_txt.setText("Address: " + address);
+        phone_txt.setText("Contact: " + phone);  // ("Phone: " + phone);  ??? çözdüm -> editprofile phone yerine mail
+        info_txt.setText("Info: " + info);
+
+        fetchUserInformation();  //get currentUserID
+
+        okbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LostAnimalViewActivity.this, LostAnimalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        deletebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Delete condition and delete logic
+                if( userid.equals(userID)) {   // User is the one who opened notice is the same AUTHORİZED TO DELETE
+
+                    // Assuming you have an ID variable
+                    String idToDelete = animal_id;
+
+                    // Construct the URI with the ID
+                    Uri deleteUri = Uri.withAppendedPath(Contract.MyEntry.CONTENT_URI, idToDelete);
+
+                    // Delete data with the specified ID
+                    int rowsDeleted = getContentResolver().delete(deleteUri, null, null);
+
+                    if (rowsDeleted > 0) {
+                        // Data deleted successfully
+                        Toast.makeText(LostAnimalViewActivity.this, "Data deleted successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Failed to delete data
+                        Toast.makeText(LostAnimalViewActivity.this, "Failed to delete data", Toast.LENGTH_SHORT).show();
+                    }
+
+                    Intent intent = new Intent(LostAnimalViewActivity.this, LostAnimalActivity.class);
+                    startActivity(intent);
+                    //finish();
+                }
+                else {
+                    Toast.makeText(LostAnimalViewActivity.this, "You are not authorized.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private void fetchUserInformation() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            String userId = firebaseUser.getUid();
+
+            userID = userId;
+        }
+    }
+
+    private void getOtherInfo(String animal_id) {
+
+        // Parse the String to an int
+        int animalId = Integer.parseInt(animal_id);
+
+        // Specify the URI for a specific item with ID 123
+        Uri uri = ContentUris.withAppendedId(Contract.MyEntry.CONTENT_URI, animalId);
+
+        // Perform a query to get the item with ID 123
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+        // Use the cursor to access the item's data
+        if (cursor != null && cursor.moveToFirst()) {
+            // Extract data from the cursor
+            //String itemName = cursor.getString(cursor.getColumnIndex(Contract.MyEntry.COLUMN_NAME));
+
+            // Get the index of the columns
+            int addressColumnIndex = cursor.getColumnIndex(Contract.MyEntry.COLUMN_ADDRESS);
+            int infoColumnIndex = cursor.getColumnIndex(Contract.MyEntry.COLUMN_INFO);
+            int phoneColumnIndex = cursor.getColumnIndex(Contract.MyEntry.COLUMN_PHONE);
+            int useridColumnIndex = cursor.getColumnIndex(Contract.MyEntry.COLUMN_USER_ID);
+
+            // Check if the columns are found
+            if (addressColumnIndex >= 0 && infoColumnIndex >= 0 && phoneColumnIndex >= 0 && useridColumnIndex >= 0 ) {
+                // Column found, get the value
+                String address2 = cursor.getString(addressColumnIndex);
+                String info2 = cursor.getString(infoColumnIndex);
+                String phone2 = cursor.getString(phoneColumnIndex);
+                String userid2 = cursor.getString(useridColumnIndex);
+
+                // Use the 'address' variable as needed
+                address = address2;
+                info = info2;
+                phone = phone2;
+                userid = userid2;
+
+            } else {
+                // Handle the case where the column is not found
+                Log.e("YourTag", "Column " + Contract.MyEntry.COLUMN_ADDRESS + " not found");
+                //.....
+            }
+
+            // ... (extract other columns as needed)
+
+            // Close the cursor when done
+            cursor.close();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Your custom behavior, if any
+        super.onBackPressed(); // This line is important to ensure the default behavior
+        Intent intent = new Intent(LostAnimalViewActivity.this, LostAnimalActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
